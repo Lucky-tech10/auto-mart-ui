9ab303bb806eef22c359199b9fbd8025254173c4
Lucky-tech10 <ezihegoodluck59@gmail.com>
2025-08-09 18:53:50 +0100

    hosted api connected

--- js/config/api.js
+++ js/config/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api/v1";
+const API_BASE_URL = "https://auto-mart-api.onrender.com/api/v1";
 
 // Token management
 const getToken = () => localStorage.getItem("authToken");
